5d0dc83ee72f52e6ce1c3b84939c9659c1e6b987
felipe <felipeavmuller@gmail.com>
2023-09-14 14:36:56 -0300

    remove project

--- main.js
+++
@@ -1,7 +0,0 @@
-const textarea = document.getElementsByTagName('textarea')[0];
-
-textarea.addEventListener('input', () => {
-  // Redefine a altura do textarea conforme digitar
-  textarea.style.height = 'auto';
-  textarea.style.height = `${textarea.scrollHeight}px`;
-})
